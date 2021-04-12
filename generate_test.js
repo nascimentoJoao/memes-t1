6633d126a7cf8242a809411f69a38eb05282876e
João Severo <severo@teialabs.com>
2021-04-12 12:58:27 -0300

    Add test mapper sketch function

--- generate_test.js
+++ generate_test.js
@@ -20,7 +20,35 @@ function parseModel(data) {
 }
 
 
+function checkFields(data) {
+    let typeRegex = /\w/
+    let defaultRegex = /\w/
+    let aliasRegex = /\w/
+    let requiredRegex = /\w/
+    let maxLengthRegex = /\w/
+    let minLengthRegex = /\w/
+    let enumRegex = /\w/
+    let minRegex = /\w/
+    let maxRegex = /\w/
+
+    let testMapper = {}
+    for (let field of data){
+        let fieldName = batata
+
+        if(!(field in testMapper)){
+            testMapper[fieldName] = {data: [], function: []}
+        }
+
+        let typeData = field.match(typeRegex)
+        if(typeData){
+            testMapper[fieldName].data = field
+            testMapper[fieldName].function = testType
+        }
+    }
+}
+
 parsed = read('./model/User.js', parseModel)
+checks = checkFields(parsed)
 
 
 //Aplicar testes unitários para os tipos
